8157d1920621f2abf075d9722dfb9482bf0f58f6
Blerina7 <tafajblerina7@gmail.com>
2026-06-01 13:22:32 +0200

    Product Controller

--- e_commerse_api/app/Http/Controllers/ProductController.php
+++ e_commerse_api/app/Http/Controllers/ProductController.php
@@ -4,45 +4,100 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    //te gjithe produktet
     public function index()
     {
-        //
+        $query=Product::query();
+       
+
+
+        $products= Product::paginate(20);
+    
+        return response() ->json($products,200 );
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   //ruaj ne db
     public function store(Request $request)
     {
-        //
+        $data=$request-> validate([
+            'name' => 'required|string|min:3',
+            'slug' => 'required|string|min:3|unique:products,slug',
+            'description' =>'string|min:3|nullable',
+            'base_price' =>'numeric|min:0|required',
+            'sale_price' =>'numeric|min:0|nullable',
+            'gender' =>'string|required',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('photo')) {
+            $path = $request->file('photo')->store('photos', 'public');
+            $data['photo'] = $path;
+        }
+
+        $product= Product::create($data);
+        return response() ->json([$product],201);
+          
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+   //trego 1 produkt specifik
+    public function show(Product $product)
     {
-        //
+       return response() ->json($product->load(['variants', 'images', 'brand', 'category']),200 );
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+   //updeton 1 produkt specifik
+    public function update(Request $request, Product $product)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string|min:3',
+            'slug' => 'required|string|unique:products,slug,' . $product->id,
+            'description' =>'string|min:3',
+            'base_price' =>'numeric|min:0',
+            'sale_price' =>'numeric|min:0',
+            'gender' =>'string',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            
+        ]  
+        );
+
+        
+        
+
+        if ($request->hasFile('photo')) {
+            if ($product->photo) {
+                Storage::disk('public')->delete($product->photo);
+            }
+            $path = $request->file('photo')->store('photos', 'public');
+
+            $data['photo'] = $path;
+        } else {
+            unset($data['photo']);
+        }
+
+        
+
+        $product ->update($data);
+        return response()->json([
+        'message'  => 'Product updated',  
+        'product'=>$product
+        ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+   //delete the product
+    public function destroy(Product $product)
+    { 
+        if ($product->photo) {
+            Storage::disk('public')->delete($product->photo);
+        }
+
+        $product->delete();
+        return response()->json([
+        'message'=>'Product was deltetd with success*'
+        ],204);
     }
 }
